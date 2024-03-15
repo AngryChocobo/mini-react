f9a54240142f6376854f4106912776877040d8fd
AngryChocobo <laoju965@gmail.com>
2024-03-15 20:49:51 +0800

    feat: createElement can return vdom and componment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
-"use strict";
-console.log("ok");
-document.body.innerText = "ok";
+const createElement = (tag, props, children) => {
+    if (typeof tag === "function") {
+        return tag(props, children);
+    }
+    const el = {
+        tag,
+        props,
+        children,
+    };
+    // console.log(type, props, children);
+    console.log(el);
+    return el;
+};
+const render = (el, container) => {
+    // container.appendChild(el);
+};
+const React = {
+    createElement,
+    render,
+};
+const App = (React.createElement("div", { draggable: true },
+    React.createElement("h2", null, "Hello React!"),
+    React.createElement("p", null, "I am a pargraph"),
+    React.createElement("input", { type: "text" })));
+const Wrapper = () => {
+    return (React.createElement("div", { className: "wrapper" },
+        React.createElement("div", { className: "content" }, "123")));
+};
+console.log(React.createElement(Wrapper, null));
+React.render(App, document.getElementById("app"));
+// const App = React.createElement("div", null, "Hello jsx!");
--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,39 @@
-console.log("ok");
-document.body.innerText = "ok";
+const createElement = (tag: string | Function, props: any, children: any) => {
+  if (typeof tag === "function") {
+    return tag(props, children);
+  }
+  const el = {
+    tag,
+    props,
+    children,
+  };
+  // console.log(type, props, children);
+  console.log(el);
+  return el;
+};
+const render = (el: any, container: HTMLElement) => {
+  // container.appendChild(el);
+};
+const React = {
+  createElement,
+  render,
+};
+const App = (
+  <div draggable>
+    <h2>Hello React!</h2>
+    <p>I am a pargraph</p>
+    <input type="text" />
+  </div>
+);
+const Wrapper = () => {
+  return (
+    <div className="wrapper">
+      <div className="content">123</div>
+    </div>
+  );
+};
+
+console.log(<Wrapper />);
+React.render(App, document.getElementById("app"));
+
+// const App = React.createElement("div", null, "Hello jsx!");
